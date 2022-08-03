6f7fe597bb97ab9fd70116f070efbbca874f5155
David Hodgson <davidhodgson.uk@gmail.com>
2022-08-03 10:27:35 +0100

    Added a second dice

--- app/src/main/java/david/hodgson/diceroller/MainActivity.kt
+++ app/src/main/java/david/hodgson/diceroller/MainActivity.kt
@@ -18,6 +18,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
+
+        // Roll the first dice
         val dice = Dice(6)
         val diceRoll = dice.roll()
 
@@ -31,6 +33,23 @@ class MainActivity : AppCompatActivity() {
             4 -> diceImage.setImageResource(R.drawable.dice_4)
             5 -> diceImage.setImageResource(R.drawable.dice_5)
             6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
+
+        // Roll the second dice
+        val dice2 = Dice(6)
+        val diceRoll2 = dice2.roll()
+
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
+        diceImage2.contentDescription = diceRoll2.toString()
+
+        // Todo; move this duplicate code into a function
+        when (diceRoll2) {
+            1 -> diceImage2.setImageResource(R.drawable.dice_1)
+            2 -> diceImage2.setImageResource(R.drawable.dice_2)
+            3 -> diceImage2.setImageResource(R.drawable.dice_3)
+            4 -> diceImage2.setImageResource(R.drawable.dice_4)
+            5 -> diceImage2.setImageResource(R.drawable.dice_5)
+            6 -> diceImage2.setImageResource(R.drawable.dice_6)
         }
     }
 }
